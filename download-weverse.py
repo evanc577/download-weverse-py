63364f71400b87da98c34627cf63242a209bf428
Evan Chang <evanc577@gmail.com>
2020-11-09 23:08:55 -0500

    Download mediaTab videos, better download paths

--- download-weverse.py
+++ download-weverse.py
@@ -19,8 +19,10 @@ except ImportError:
 class WeverseUrls:
     info = 'https://weversewebapi.weverse.io/wapi/v1/communities/info'
     artistTab = 'https://weversewebapi.weverse.io/wapi/v1/communities/{}/posts/artistTab?pageSize={}&from={}'
+    mediaTab = 'https://weversewebapi.weverse.io/wapi/v1/stream/community/{}/mediaTab?pageSize={}&from={}'
     toFans = 'https://weversewebapi.weverse.io/wapi/v1/stream/community/{}/toFans?pageSize={}&from={}'
     post = 'https://weversewebapi.weverse.io/wapi/v1/communities/{}/posts/{}'
+    videoDash = 'https://cdn-media.weverse.io/video{}/DASH.mpd'
 
 config = {}
 
@@ -49,10 +51,8 @@ def dwexit(code):
     exit(code)
 
 
-def download_post(artist_id, post_type, config, post, combine_categories=False):
+def download_post(artist_id, download_dir, config, post):
     with tempfile.TemporaryDirectory() as temp_dir:
-        if combine_categories:
-            post_type = ''
         post_id = post['id']
         user = post['communityUser']['profileNickname']
         user_image_url = post['communityUser']['profileImgPath']
@@ -62,7 +62,7 @@ def download_post(artist_id, post_type, config, post, combine_categories=False):
         date_str = dt.strftime("%y%m%d")
         ts_str = str(dt)
         filename_prefix = f'{date_str}_{post_id}_{user}'
-        dir_path = os.path.join(config['downloadPath'], post_type, filename_prefix)
+        dir_path = os.path.join(download_dir, filename_prefix)
 
         # check if already downloaded
         if os.path.exists(dir_path):
@@ -105,7 +105,7 @@ def download_post(artist_id, post_type, config, post, combine_categories=False):
         os.utime(temp_dir, (ts, ts))
 
         # atomically copy to destination
-        path = os.path.join(config['downloadPath'], post_type)
+        path = download_dir
         shutil.move(temp_dir, path)
         temp_dir2 = os.path.join(path, os.path.basename(temp_dir))
         os.rename(temp_dir2, dir_path)
@@ -128,18 +128,40 @@ def write_content(path, post_id, user, body, ts_str, ts, config):
         print(f'{body}', file=f)
     os.utime(path, (ts, ts))
 
+
+def download_videos(artist_id, config, media):
+    if media['type'].lower() != 'video':
+        return
+    if 'extVideoPath' in media:
+        return
+    media_id = media['id']
+    vid_id = media['video']['id']
+    title = media['title']
+    dt = datetime.strptime(media['createdAt'], '%Y-%m-%dT%H:%M:%S%z')
+    ts = dt.timestamp()
+    date_str = dt.strftime("%y%m%d")
+    ts_str = str(dt)
+    filename = f'{date_str}_{media_id}_{title}.mp4'
+
+    dir_path = config['videosDownloadPath']
+    if os.path.exists(os.path.join(dir_path, filename)):
+        return
+
+    # download video via youtube-dl
+    import youtube_dl
+    print(f'Downloading {filename}')
+    ydl_opts = {
+        'outtmpl': os.path.join(dir_path, filename),
+        'cookiefile': config['cookiesFile'],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([WeverseUrls.videoDash.format(vid_id)])
+
+
 def main():
     # read config
     with open('config.yml', 'r') as f:
         config = yaml.load(f, Loader=Loader)
-    combine_categories = config.get('combine_categories', False)
-    if combine_categories:
-        os.makedirs(config['downloadPath'], exist_ok=True)
-    else:
-        artist_path = os.path.join(config['downloadPath'], 'artist')
-        moments_path = os.path.join(config['downloadPath'], 'moments')
-        os.makedirs(artist_path, exist_ok=True)
-        os.makedirs(moments_path, exist_ok=True)
     num_processes = int(config.get('numProcesses', None))
 
     s = init_session(config)
@@ -152,12 +174,9 @@ def main():
         if config['artist'].lower() == community['name'].lower():
             artist_id = community['id']
 
-
-    download_kwargs = {
-        'combine_categories': combine_categories,
-    }
-
     # get posts
+    artist_path = config['artistDownloadPath']
+    os.makedirs(artist_path, exist_ok=True)
     print('Downloading posts...')
     last_id = ''
     posts_remain = config.get('recentArtist', None)
@@ -173,7 +192,7 @@ def main():
             posts_remain -= len(posts)
         ended = r.json()['isEnded']
         # download posts
-        func = partial(download_post, artist_id, 'artist', config, **download_kwargs)
+        func = partial(download_post, artist_id, artist_path, config)
         with Pool(num_processes) as pool:
             pool.map(func, posts)
         if ended or (posts_remain is not None and posts_remain <= 0):
@@ -181,6 +200,8 @@ def main():
         last_id = r.json()['lastId']
 
     # get moments
+    moments_path = config['momentsDownloadPath']
+    os.makedirs(moments_path, exist_ok=True)
     print('Downloading moments...')
     last_id = ''
     moments_remain = config.get('recentMoments', None)
@@ -196,12 +217,38 @@ def main():
             moments_remain -= len(moments)
         ended = r.json()['isEnded']
         # download moments
-        func = partial(download_post, artist_id, 'moments', config, **download_kwargs)
+        func = partial(download_post, artist_id, moments_path, config)
         with Pool(num_processes) as pool:
             pool.map(func, moments)
         if ended or (moments_remain is not None and moments_remain <= 0):
             break
         last_id = r.json()['lastId']
+
+    # get videos
+    if config.get('downloadVideos', False):
+        videos_path = os.path.join(config['videosDownloadPath'])
+        os.makedirs(videos_path, exist_ok=True)
+        print('Downloading videos...')
+        last_id = ''
+        videos_remain = config.get('recentVideos', None)
+        while True:
+            if videos_remain is not None:
+                url = WeverseUrls.mediaTab.format(artist_id, videos_remain, last_id)
+            else:
+                url = WeverseUrls.mediaTab.format(artist_id, 100, last_id)
+            print(f'GET {url}')
+            r = s.get(url)
+            videos = r.json()['medias']
+            if videos_remain is not None:
+                videos_remain -= len(videos)
+            ended = r.json()['isEnded']
+            # download videos
+            func = partial(download_videos, artist_id, config)
+            with Pool(num_processes) as pool:
+                pool.map(func, videos)
+            if ended or (videos_remain is not None and videos_remain <= 0):
+                break
+            last_id = r.json()['lastId']
 
 
 if __name__ == '__main__':
